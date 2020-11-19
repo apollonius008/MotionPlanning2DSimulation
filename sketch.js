6949ef10524b379b68366bd63584ee8a0ff3c8bd
Sarit Kumar Das <quantumalex008@gmail.com>
2020-11-19 20:14:24 +0530

    MINOR STYLE CHANGES

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let canvas_panel;
 let canvas;
+
 function setup() {
 	canvas_panel = document.getElementById('canvas-window');
 	canvas = createCanvas(canvas_panel.clientWidth, canvas_panel.clientHeight);
